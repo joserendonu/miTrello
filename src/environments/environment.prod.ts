ded61cef41e22dc8bd390bd2ac32e651cfe977b2
joserendonu <angelemilio4444@gmail.com>
2026-03-22 22:27:48 -0500

    apk con cordova

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBB8ZbHN6GJqbDkbjDcwV2355HfODMCLds",
+  authDomain: "mitrellobd.firebaseapp.com",
+  projectId: "mitrellobd",
+  storageBucket: "mitrellobd.firebasestorage.app",
+  messagingSenderId: "681808658263",
+  appId: "1:681808658263:web:b79e5a1c03fa3c9f788812"
+  // measurementId: "G-JN0TNJ55XL"
+};
+
+
+
 export const environment = {
-  production: true
+  production: false,
+  firebase: firebaseConfig
 };
